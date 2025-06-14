458aa162f988505cea6bcfc2f9a8b84db7e67b3b
eunchan96 <taegwon02@gmail.com>
2025-06-14 15:25:50 +0900

    t5~7 b

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,14 +3,22 @@ package com.back;
 public class Calc {
     static int run(String expression){
         expression = expression.trim();
+        String[] exp = new String[2];
 
         int add = expression.indexOf("+");
+        if(add != -1) {
+            exp[0] = expression.substring(0, add).trim();
+            exp[1] = expression.substring(add + 1).trim();
+            return Integer.parseInt(exp[0]) + Integer.parseInt(exp[1]);
+        }
 
-        if(add == -1) return Integer.parseInt(expression);
-        String[] exp = new String[2];
-        exp[0] = expression.substring(0, add).trim();
-        exp[1] = expression.substring(add + 1).trim();
+        int sub = expression.indexOf("-");
+        if(sub != -1){
+            exp[0] = expression.substring(0, sub).trim();
+            exp[1] = expression.substring(sub + 1).trim();
+            return Integer.parseInt(exp[0]) - Integer.parseInt(exp[1]);
+        }
 
-        return Integer.parseInt(exp[0]) + Integer.parseInt(exp[1]);
+        return Integer.parseInt(expression);
     }
 }
