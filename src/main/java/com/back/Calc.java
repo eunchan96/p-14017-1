cc520ce6d7c4cf1afe52bd3b24514339c772e7d9
eunchan96 <taegwon02@gmail.com>
2025-06-14 15:19:51 +0900

    t1~4 b

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,5 +2,15 @@ package com.back;
 
 public class Calc {
     static int run(String expression){
+        expression = expression.trim();
+
+        int add = expression.indexOf("+");
+
+        if(add == -1) return Integer.parseInt(expression);
+        String[] exp = new String[2];
+        exp[0] = expression.substring(0, add).trim();
+        exp[1] = expression.substring(add + 1).trim();
+
+        return Integer.parseInt(exp[0]) + Integer.parseInt(exp[1]);
     }
 }
