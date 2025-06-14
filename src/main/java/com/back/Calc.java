f8249ceb0271b448bb671c0720974e153443dd78
eunchan96 <taegwon02@gmail.com>
2025-06-14 16:02:16 +0900

    t13~14 g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -5,6 +5,13 @@ public class Calc {
         expression = expression.trim();
         String[] exp = new String[2];
 
+        int mul = expression.indexOf("*");
+        if(mul != -1) {
+            exp[0] = expression.substring(0, mul).trim();
+            exp[1] = expression.substring(mul + 1).trim();
+            return run(exp[0]) * run(exp[1]);
+        }
+
         int add = expression.indexOf("+");
         if(add != -1) {
             exp[0] = expression.substring(0, add).trim();
@@ -13,17 +20,14 @@ public class Calc {
         }
 
         int sub = expression.lastIndexOf("-");
+        if(sub == 0) {
+            exp[1] = expression.substring(1).trim();
+            return -run(exp[1]);
+        } else
         if(sub != -1){
             exp[0] = expression.substring(0, sub).trim();
             exp[1] = expression.substring(sub + 1).trim();
             return run(exp[0]) - run(exp[1]);
-        }
-
-        int mul = expression.indexOf("*");
-        if(mul != -1) {
-            exp[0] = expression.substring(0, mul).trim();
-            exp[1] = expression.substring(mul + 1).trim();
-            return run(exp[0]) * run(exp[1]);
         }
 
         return Integer.parseInt(expression);
